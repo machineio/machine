e3f14b9c5d7111b16974f25ab9c592cbb39d5c77
dmontero <dmontero@spartanapproch.com>
2017-11-20 17:46:21 -0600

    contact form changes

--- static/js/views/contact.js
+++ static/js/views/contact.js
@@ -1,63 +1,64 @@
 fun.views.contact = Backbone.View.extend({
 
-  /*
-  * Bind the event functions to the different HTML elements
-  */
-  events : {
-    'click #submit': 'submit'
-  },
-  
-  /*
-  * Class constructor
-  */
-  initialize : function(options) {
-    fun.containers.contact = this.$el;
-  },
-  
-  /*
-  * Render the branches view
-  */
-  render : function(){
-    if (!this.$el.html()){
-      var template = _.template(fun.utils.getTemplate(fun.conf.templates.contact));
-      this.$el.html(template);
-    }
-    this.$el.removeClass("hide").addClass("show");
-  },
+    /*
+     * Bind the event functions to the different HTML elements
+     */
+    events: {
+        'click #submit': 'submit'
+    },
 
-  submit : function(event) {
-    event.preventDefault();
-    $('#frm').validate();
-    // check jquery validation
-      if ($('#frm').valid()) {
-      var model = new fun.models.Email();
-      var data, confirmation;
-      data = {
-        account: 'spartaadmin',
-        subject: 'Information requested - Contact page form',
-        source: $('#email').val(),
-        destination: 'bchassoul@spartanapproach.com',
-        html: 'Message from: ' + $('#first_name').val() + ' ' + $('#last_name').val() + ', email: ' + $('#email').val() + ', phone: ' + $('#phone').val() + ', message: ' + $('#message').val()
-        };
+    /*
+     * Class constructor
+     */
+    initialize: function(options) {
+        fun.containers.contact = this.$el;
+    },
 
-      confirmation = {
-        account: 'spartaadmin',
-        subject: 'Thank you for your trusting Code Machine',
-        source: ' info@codemachine.io',
-        destination: $('#email').val(),
-        html: 'Thank for getting in touch with Code Machine Team. Our compromise is to find the best practices and methologies to make your company more sucessful. Professional Developers will be sending you an email shortly to discuss for a better future in your businesses.'
-        };
+    /*
+     * Render the branches view
+     */
+    render: function() {
+        if (!this.$el.html()) {
+            var template = _.template(fun.utils.getTemplate(fun.conf.templates.contact));
+            this.$el.html(template);
+        }
+        this.$el.removeClass("hide").addClass("show");
+    },
 
-      model.url = fun.conf.urls.emails
-      model.save(confirmation);    
-      model.save(data, {success: function (model, respose) {
-        $('#frm').addClass('display-none');
-        $('#thankyou').html('We will be in touch soon');
-        $('#contact-wrapper').attr('style', 'margin-bottom: 20%');
-        console.log('Email sent');      
-      }});
-    } else {
-      console.log('missing data');
-    }
-  },
+    submit: function(event) {
+        event.preventDefault();
+        $('#frm').validate();
+        // check jquery validation
+        if ($('#frm').valid()) {
+            var model = new fun.models.Email();
+            var data, confirmation;
+            data = {
+                account: 'spartaadmin',
+                subject: 'Information requested - Contact page form',
+                source: $('#email').val(),
+                destination: 'bchassoul@spartanapproach.com',
+                html: 'Message from: ' + $('#first_name').val() + ' ' + $('#last_name').val() + ', email: ' + $('#email').val() + ', phone: ' + $('#phone').val() + ', message: ' + $('#message').val()
+            };
+
+            confirmation = {
+                account: 'spartaadmin',
+                subject: 'Thank you for your trusting Code Machine',
+                source: ' info@codemachine.io',
+                destination: $('#email').val(),
+                html: 'Thank for getting in touch with Code Machine Team. Our compromise is to find the best practices and methologies to make your company more sucessful. Professional Developers will be sending you an email shortly to discuss for a better future in your businesses.'
+            };
+
+            model.url = fun.conf.urls.emails
+            model.save(confirmation);
+            model.save(data, {
+                success: function(model, respose) {
+                    $('#frm').hide();
+                    $('#thankyou').html('We will be in touch soon');
+                    console.log('Email sent');
+                }
+            });
+        } else {
+            console.log('missing data');
+        }
+    },
 });
