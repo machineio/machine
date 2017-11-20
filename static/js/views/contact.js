2ce1418e1383ce9f90e926b8e9371ae18285c336
dmontero <dmontero@spartanapproch.com>
2017-11-20 09:43:00 -0600

    validations

--- static/js/views/contact.js
+++ static/js/views/contact.js
@@ -27,32 +27,37 @@ fun.views.contact = Backbone.View.extend({
 
   submit : function(event) {
     event.preventDefault();
-    var model = new fun.models.Email();
-    var data, confirmation;
-    data = {
-      account: 'spartaadmin',
-      subject: 'Information requested - Contact page form',
-      source: $('#email').val(),
-      destination: 'bchassoul@spartanapproach.com',
-      html: 'Message from: ' + $('#first_name').val() + ' ' + $('#last_name').val() + ', email: ' + $('#email').val() + ', phone: ' + $('#phone').val() + ', message: ' + $('#message').val()
-      };
+    $('#frm').validate();
+    // check jquery validation
+      if ($('#frm').valid()) {
+      var model = new fun.models.Email();
+      var data, confirmation;
+      data = {
+        account: 'spartaadmin',
+        subject: 'Information requested - Contact page form',
+        source: $('#email').val(),
+        destination: 'bchassoul@spartanapproach.com',
+        html: 'Message from: ' + $('#first_name').val() + ' ' + $('#last_name').val() + ', email: ' + $('#email').val() + ', phone: ' + $('#phone').val() + ', message: ' + $('#message').val()
+        };
 
-    confirmation = {
-      account: 'spartaadmin',
-      subject: 'Thank you for your recent insurance inquiry',
-      source: ' info@codemachine.io',
-      destination: $('#email').val(),
-      html: 'testing'
-      };
+      confirmation = {
+        account: 'spartaadmin',
+        subject: 'Thank you for your trusting Code Machine',
+        source: ' info@codemachine.io',
+        destination: $('#email').val(),
+        html: 'Thank for getting in touch with Code Machine Team. Our compromise is to find the best practices and methologies to make your company more sucessful. Professional Developers will be sending you an email shortly to discuss for a better future in your businesses.'
+        };
 
-    model.url = fun.conf.urls.emails
-    model.save(confirmation);
-    model.save(data, {success: function (model, respose) {
-        console.log('yay')
-    }});
-    console.log(data);
-    console.log(confirmation);
-    console.log(model);
+      model.url = fun.conf.urls.emails
+      model.save(confirmation);    
+      model.save(data, {success: function (model, respose) {
+        $('#frm').addClass('display-none');
+        $('#thankyou').html('We will be in touch soon');
+        $('#contact-wrapper').attr('style', 'margin-bottom: 20%');
+        console.log('Email sent');      
+      }});
+    } else {
+      console.log('missing data');
+    }
   },
-
 });
